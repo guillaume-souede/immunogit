f69e324c66756e3f300f9df32ae3af7f6dc44f08
Guillaume SOUEDE <guillaume.souede@univ-tlse3.fr>
2025-01-28 21:52:16 +0100

    filepaths fixed libraries fixed BioServices taken use of models added

--- src/common_immunogit.py
+++ src/common_immunogit.py
@@ -4,29 +4,46 @@ MODULES
 import asyncio
 from playwright.async_api import async_playwright
 import nest_asyncio
+from bs4 import BeautifulSoup
+import re
 import os
+import json
 import subprocess
 import sys
+from pprint import pprint
 import zipfile as z
 import requests
-import json
-from re import *
-from bs4 import BeautifulSoup
+import git
+from pathlib import Path
+
+try:
+    from bioservices import BioModels
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "bioservices"])
+    from bioservices import BioModels
+
+""" 
+CONSTANTS 
 """
-PATHS
+BLUE = '\033[94m'
+RED = '\033[91m'
+RESET = '\033[0m'
+
+""" 
+PATHS 
 """
-current_path = os.getcwd()
-root_path = os.path.abspath(os.path.join(current_path, ".."))
+repo = git.Repo('.', search_parent_directories=True)
+root_path = repo.working_tree_dir
+print(root_path)
+bm_sbml_path = Path(root_path) / "models" / "BioModels" / "SBML"
 
 os.makedirs(os.path.join(root_path, "models/BioModels/SBML"), exist_ok=True)
 os.makedirs(os.path.join(root_path, "models/Reactome/SBML"), exist_ok=True)
 os.makedirs(os.path.join(root_path, "models/Reactome/SBGN"), exist_ok=True)
 os.makedirs(os.path.join(root_path, "metadata"), exist_ok=True)
-print("Structure : Done.")
+print("Structure Ok.")
 
-"""
-COMMON VARIABLES
-"""
-BLUE = '\033[94m'
-RED = '\033[91m'
-RESET = '\033[0m'
+md_path = Path(repo.working_tree_dir) / "metadata"
+all_md_path = md_path / "metadata_all.json"
+bio_md_path = md_path / "metadata_BioModels.json"
+rea_md_path = md_path / "metadata_Reactome.json"
