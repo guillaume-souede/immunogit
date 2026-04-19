06e5fec63a098dce31f6606a8be85dd4020ab209
Guillaume SOUÈDE <84975628+guillaume-souede@users.noreply.github.com>
2026-04-19 21:15:51 +0200

    Improve make.bat error handling for Sphinx
    Updated error messages and command checks for sphinx-build.

--- docs/make.bat
+++ docs/make.bat
@@ -1,35 +1,41 @@
 @ECHO OFF
+setlocal EnableExtensions EnableDelayedExpansion
 
-pushd %~dp0
+pushd "%~dp0" || exit /b 1
 
 REM Command file for Sphinx documentation
 
-if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
-)
-set SOURCEDIR=.
-set BUILDDIR=_build
+if "%SPHINXBUILD%"=="" set "SPHINXBUILD=sphinx-build"
+set "SOURCEDIR=."
+set "BUILDDIR=_build"
 
-%SPHINXBUILD% >NUL 2>NUL
+REM Check sphinx-build exists
+"%SPHINXBUILD%" --version >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
-	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
-	echo.may add the Sphinx directory to PATH.
+	echo.The 'sphinx-build' command was not found.
+	echo.Install it with: python -m pip install sphinx
+	echo.Or set SPHINXBUILD to the full path of sphinx-build.exe
 	echo.
-	echo.If you don't have Sphinx installed, grab it from
-	echo.https://www.sphinx-doc.org/
+	popd
+	endlocal
 	exit /b 1
 )
 
-if "%1" == "" goto help
+if "%~1"=="" goto help
 
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
-goto end
+REM Target is first arg; pass the rest through to sphinx via SPHINXOPTS
+set "TARGET=%~1"
+shift
+
+"%SPHINXBUILD%" -M "%TARGET%" "%SOURCEDIR%" "%BUILDDIR%" %SPHINXOPTS% %O% %*
+set "RC=%ERRORLEVEL%"
+
+popd
+endlocal & exit /b %RC%
 
 :help
-%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
-
-:end
+"%SPHINXBUILD%" -M help "%SOURCEDIR%" "%BUILDDIR%" %SPHINXOPTS% %O%
+set "RC=%ERRORLEVEL%"
 popd
+endlocal & exit /b %RC%
